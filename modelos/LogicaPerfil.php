922ef1b3981993cc5590b3ab8e790ec1e19ec14c
velasquezmon <juseve200@gmail.com>
2015-02-26 20:36:22 -0500

    Validacion Busqueda Agregada

--- modelos/LogicaPerfil.php
+++ modelos/LogicaPerfil.php
@@ -10,7 +10,9 @@
 	{
 		
 		private $coordinadorP; //CoordinadorPerfil
-		private $response;
+		private $responseModificar;
+		private $responseRegistrar;
+		private $responseConsultar;
 		private $perfil;
 		private $validar; //Validaciones
 
@@ -25,58 +27,72 @@
 			$this->coordinadorP = $coordinador;
 		}
 
-		public function validarRegistroPerfil($nombre, $permisoGestionarUsuarios, 
+		public function validarRegistrarPerfil($nombre, $permisoGestionarUsuarios, 
 			$permisoVender, $permisoGestionarPerfiles)
 		{
 			if ($nombre =="" or $permisoGestionarUsuarios =="" 
 				or $permisoVender =="" or $permisoGestionarPerfiles =="") {
-				$this->response[0] = "Todos los campos son requeridos";
+				$this->responseRegistrar[0] = "Todos los campos son requeridos";
 			}
 			if ($this->validar->esMayor($nombre,30)) {
-				$this->response[1] = "El nombre debe contener maximo 30 caracteres";
+				$this->responseRegistrar[1] = "El nombre debe contener maximo 30 caracteres";
 			}
 			if ($this->validar->esMenor($nombre,4)){
-				$this->response[2] = "El nombre debe contener minimo 4 caracteres";
+				$this->responseRegistrar[2] = "El nombre debe contener minimo 4 caracteres";
 			}
 			if (!($this->validar->esAlfabetico($nombre))){
-				$this->response[3] = "El nombre debe ser alfabetico";
+				$this->responseRegistrar[3] = "El nombre debe ser alfabetico";
 			}
-			if (empty($this->response)) 
+			if (!empty($this->perfil->buscarPerfil($nombre))) 
+			{
+				$this->responseRegistrar[4] = "El nombre ya existe";
+			}
+			if (empty($this->responseRegistrar)) 
 			{
 				$this->perfil->registrarPerfil($nombre, $permisoGestionarUsuarios, 
 					$permisoVender, $permisoGestionarPerfiles);
-				$this->response[0] = "Perfil creado con exito";			
+				$this->responseRegistrar[0] = "Perfil creado con exito";			
 			}	
-			return $this->response;
+			return $this->responseRegistrar;
 		}
 
 		public function validarModificarPerfil($nombre, $permisoGestionarUsuarios, 
 			$permisoVender, $permisoGestionarPerfiles)
 		{
 			if ($nombre =="") {
-				$this->response[0] = "Ingrese un nombre";
+				$this->responseModificar[0] = "Ingrese un nombre";
 			}
 			if ($this->validar->esMayor($nombre,30)) {
-				$this->response[1] = "El nombre debe contener maximo 30 caracteres";
+				$this->responseModificar[1] = "El nombre debe contener maximo 30 caracteres";
 			}
 			if ($this->validar->esMenor($nombre,4)){
-				$this->response[2] = "El nombre debe contener minimo 4 caracteres";
+				$this->responseModificar[2] = "El nombre debe contener minimo 4 caracteres";
 			}
 			if (!($this->validar->esAlfabetico($nombre))){
-				$this->response[3] = "El nombre debe ser alfabetico";
+				$this->responseModificar[3] = "El nombre debe ser alfabetico";
 			}
-			if (empty($this->response)) 
+			if (empty($this->responseModificar)) 
 			{
 				$this->perfil->modificarPerfil($nombre, $permisoGestionarUsuarios, 
 					$permisoVender, $permisoGestionarPerfiles);
-				$this->response[0] = "Perfil modificado con exito";			
+				$this->responseModificar[0] = "Perfil modificado con exito";			
 			}	
-			return $this->response;
+			return $this->responseModificar;
+		}
+
+		public function validarConsultarPerfil($nombre)
+		{
+			if(empty($this->perfil->buscarPerfil($nombre)))
+			{
+				$this->responseConsultar[0] = "No exite el perfil " .$nombre;
+			}
+			return $this->responseConsultar;
 		}
 	}
-	/*$logica = new LogicaPerfil();
-	$status = $logica->validarModificarPerfil("root",0,0,0);
+	$logica = new LogicaPerfil();
+	//$status = $logica->validarModificarPerfil("root",0,0,0);
+	$status = $logica->validarRegistrarPerfil("Admin",1,1,1);
 	foreach ($status as $key) {
 		echo $key;
-	}*/
+	}
 ?>
