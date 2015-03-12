a09dea4645753ec6cc3f5d3d72048278ec194f82
velasquezmon <juseve200@gmail.com>
2015-03-11 23:02:32 -0500

    Validaciones hechas con REGEX

--- modelos/Validaciones.php
+++ modelos/Validaciones.php
@@ -5,44 +5,28 @@
 	class Validaciones 
 	{
 
-		private $numbers = array('0','1','2','3','4','5','6','7','8','9');
-		private $letters = array(' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z','á','é','í','ó','ú',
-								'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z','Á','É','Í','Ó','Ú',);
-
-		public function esAlfabetico($valor)
+		public function esAlfabetico($parametro)
 		{
-			$tam = strlen($valor);
-			$salida1 = false;
-			$salida2 = false;
-			// El primer 'for' recorre todo el 'valor' (letra por letra)
-			for ($i=0; $i < $tam; $i++) {
-				$salida2 = false;
-				// Segundo 'for' que recorre todo el arreglo de letras y verifica
-				// si coinciden, en caso de que no, salta fuera de los dos 'for's 
-				// y devolverá un falso
-				foreach ($this->letters as $key) {
-					if(substr($valor,$i,1) == $key){
-						$salida2 = true;
-						break;
-					}
-				}
-				if(!$salida2)
-					break;
-		}
-		if($salida2)
-			$salida1 = true;
-
-		return $salida1;
+			return preg_match('/^[a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑüÜ ]*$/', $parametro);
 		}
 
-		public function esNumerico($valor)
+		public function esNumerico($parametro)
 		{
-			return ctype_digit($valor);
+			return preg_match('/^[0-9]*$/', $parametro);
 		}
 
-		public function esAlfanumerico($valor)
+		public function esAlfanumerico($parametro)
 		{
-			return ctype_alnum($valor);
+			return preg_match('/^[a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑüÜ0-9]*$/', $parametro);
+		}
+
+		public function validarEmail($parametro)
+		{	
+			if ((strlen($parametro) <6 ) || (strlen($parametro) > 60)){
+				return preg_match('/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})$/', $parametro);
+			}else{
+				return 0;
+			}
 		}
 
 		public function esMayor($valor, $tamanoMax)
