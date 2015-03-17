44b266a12d953101816a1c8e6733f72daebe1d31
Sebastian <juseve200@gmail.com>
2015-03-16 21:28:30 -0500

    Inicio del refactoring de Perfiles

---
+++ modelos/ValidarLogin.php
@@ -0,0 +1,25 @@
+<?php 
+	/**
+	* 
+	*/
+	class ValidarLogin 
+	{
+		private responseLogin;
+
+		function __construct()
+		{
+			# code...
+		}
+
+		public function ValidarLogin($usuario, $password)
+		{
+			# code...
+		}
+
+		public function getResponse()
+		{
+			return $this->responseLogin
+		}
+	}
+	
+?>
